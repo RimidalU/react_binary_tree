52625e84a33affb261e7e022c134f049dfaab3d9
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-02-24 23:30:44 +0300

    feat: add binary tree creator

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import Graph from './components/Graph/Graph'
 import Header from './components/Header/Header'
+import Tree from './helpers/createBinaryTree'
+
+const tree = new Tree()
 
 function App() {
 	return (
---
+++ src/helpers/createBinaryTree.ts
@@ -0,0 +1,53 @@
+class TreeNode {
+  value: number
+  left: TreeNode | null
+  right: TreeNode | null
+
+  constructor(item: number) {
+    this.value = item
+    this.left = null
+    this.right = null
+  }
+}
+
+class Tree {
+  root: TreeNode | null
+
+  constructor() {
+    this.root = null  //initial tree
+  }
+
+  addItem(item: number) {
+    const newNode = new TreeNode(item)
+    if (this.root === null) {
+      this.root = newNode   // initial tree root
+    } else {
+      this._addItem(this.root, newNode)  // add new node to tree
+    }
+  }
+
+  _addItem(currentNode: TreeNode, newNode: TreeNode) {
+
+    if (newNode.value < currentNode.value) {
+      if (currentNode.left === null) {
+        currentNode.left = newNode
+      } else {
+        this._addItem(currentNode.left, newNode)
+      }
+    }
+
+    if (newNode.value > currentNode.value) {
+      if (currentNode.right === null) {
+        currentNode.right = newNode
+      } else {
+        this._addItem(currentNode.right, newNode)
+      }
+    }
+
+    if (newNode.value === currentNode.value) {
+      console.log('ups! Duplicate((')
+    }
+  }
+}
+
+export default Tree
